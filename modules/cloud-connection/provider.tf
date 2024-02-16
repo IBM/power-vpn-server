b764bb465c9f5375b848d4a96338d0a2e2e68ff5
Travis CI User <travis@example.org>
2024-02-16 14:38:40 +0000

    v0.2.1 release

--- modules/cloud-connection/provider.tf
+++ modules/cloud-connection/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.58.1"
+      version = "1.62.0"
     }
     time = {
       source  = "hashicorp/time"
--- modules/transit/provider.tf
+++ modules/transit/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.58.1"
+      version = "1.62.0"
     }
   }
   required_version = ">= 1.0.0"
